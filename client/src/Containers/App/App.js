a446815ad76b8331afe29b19abc90f87657c4188
Sarfaraz Ahmed <imsarfy@gmail.com>
2019-03-26 00:55:54 +0530

    Initial Commit

---
+++ client/src/Containers/App/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <h1>Hello World</h1>
+      </Router>
+    );
+  }
+}
+
+export default App;
